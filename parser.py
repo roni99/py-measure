98eb0b067f6b68df2ee93fe535709552b2cf238f
rl3262 <rl3262@stern.nyu.edu>
2019-08-16 15:12:17 -0400

    (0.2) JSON object representing html; retaining unstructured_descriptions enabled

--- parser.py
+++ parser.py
@@ -1,9 +1,8 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 import json
 
 url = "https://www.nyu.edu/about/policies-guidelines-compliance/policies-and-guidelines/data-and-system-security-measures.html"
-
 
 def main():
 
@@ -25,28 +24,53 @@ def main():
 def get_measures(root, title, name="h2"):
     return root.find(lambda tag : tag.string == title and tag.name == name)
 
-# assumption is every child in list_tag is either an li containing a b and an ol or an li containing a b
+# assumption is every child in list_tag is either:
+# <li> <b/><ol/> text </li>, in which case, turn the pair <b/><ol/> into an entry,
+# or <li> <ol/> </li>, in which case, turn the ol into a list,
+# or <li> text </li>, in which case, return the text
 def ol_to_list(root):
-    # When calling on child.ol (li.ol), return { title: li.b, value: (list || string) }
-    # What's left is to append all to list and return
+    # When calling on child.ol (li.ol), return { title: li.b, value: (list from li.ol || string) }
+    # What's left is to append that object to list and return list
     if root is not None:
         this_list = []
 
         for li in root.children:
             if (li.name is not None):
-                item = dict()
+
+                item = None
 
                 has_title = li.b is not None
-
-                item["title"] = li.b.string if has_title else str(len(this_list))
-
                 has_sublist = li.ol is not None
-                non_title_strings = ""
-                if (not has_sublist):
-                    non_title_strings += "".join([(tag.string if tag.string else str(tag)) for tag in li.children if tag.name != "b"])
-                    non_title_strings = "".join([c for c in non_title_strings if c.isalnum() or c.isspace()])
 
-                item["value"] = ol_to_list(li.ol) if has_sublist else non_title_strings
+                alphanum_only = lambda string : "".join([c for c in string if c.isalnum() or c.isspace()])
+
+                if (has_title and has_sublist):
+
+                    item = {
+                        "title": alphanum_only(li.b.string).strip(),
+                        "value": ol_to_list(li.ol)
+                    }
+                    # unstructured descriptions make parsing ambiguous, so treat them as part of the title;
+                    # -none of them are meaningful at the moment, as they are all followed by more descriptive lists
+                    unstructured_description = "".join([string for string in li.contents if isinstance(string, NavigableString)])
+                    item["title"] += unstructured_description
+
+                elif (has_title):
+                    non_title_text = ""
+                    if (not has_sublist):
+                        non_title_text += "".join([(tag.string if tag.string else str(tag)) for tag in li.children if tag.name != "b"])
+                        non_title_text = alphanum_only(non_title_text).strip()
+                    item = {
+                        "title": alphanum_only(li.b.string).strip(),
+                        "value": non_title_text
+                    }
+
+                elif (has_sublist):
+                    item = ol_to_list(li.ol)
+
+                else:
+                    item = li.get_text()
+
                 this_list.append(item)
 
         return this_list
